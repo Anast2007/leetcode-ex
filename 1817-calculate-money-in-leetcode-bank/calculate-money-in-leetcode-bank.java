9bc9fda46962bae4830c948b0f4bc3d1faa63bcb
Anast2007 <manast5345@gmail.com>
2026-03-04 09:55:19 +0530

    Time: 2 ms (24.67%) | Memory: 42.3 MB (26.34%) - LeetSync

---
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int totalMoney(int n) {
+        int total=0;
+        for(int i=0;i<n;i++){
+            total=total+(i/7)+(i%7)+1;
+        }
+        return total;
+    }
+}
