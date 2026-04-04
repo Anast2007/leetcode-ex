23d6b040fdb0a11e20b3263de73c88c8c1e9d7a1
Anast2007 <manast5345@gmail.com>
2026-04-04 20:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (75.45%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -7,21 +7,13 @@ class Solution {
         int water=0;
         while(left<right){
             if(height[left]<height[right]){
-                if(height[left]>=leftMax){
-                    leftMax=height[left];
-                }
-                else{
-                    water += leftMax-height[left];
-                }
+                if(height[left]>=leftMax) leftMax=height[left];
+                else water += leftMax-height[left];
                 left++;
             }
             else{
-                if(height[right]>=rightMax){
-                    rightMax=height[right];
-                }
-                else{
-                    water += rightMax-height[right];
-                }
+                if(height[right]>=rightMax) rightMax=height[right];
+                else water += rightMax-height[right];
                 right--;
             }
         }
