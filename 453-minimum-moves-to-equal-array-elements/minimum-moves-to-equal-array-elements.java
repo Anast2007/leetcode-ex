7af265a288a545b70830db11f725cd29f5faef56
Anast2007 <manast5345@gmail.com>
2026-08-19 19:12:32 +0530

    Time: 1 ms (100.00%) | Memory: 47.1 MB (84.57%) - LeetSync

--- 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
+++ 453-minimum-moves-to-equal-array-elements/minimum-moves-to-equal-array-elements.java
@@ -2,11 +2,13 @@ class Solution {
     public int minMoves(int[] nums) {
         int min = nums[0];
         int ans = 0;
-        for (int n : nums) {
-            min = Math.min(min, n);
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+            }
         }
-        for (int n : nums) {
-            ans += n - min;
+        for (int i = 0; i < nums.length; i++) {
+            ans += nums[i] - min;
         }
         return ans;
     }
