a0e16af6d2124e6b054d99da76713d5d658a7d76
Anast2007 <manast5345@gmail.com>
2026-09-10 18:56:47 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (8.73%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int open = 0, ans = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') open++;
+            else if (open > 0) open--;
+            else ans++;
+        }
+        return ans + open;
+    }
+}
