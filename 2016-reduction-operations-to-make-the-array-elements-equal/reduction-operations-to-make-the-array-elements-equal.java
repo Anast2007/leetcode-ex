6de4c7bd16fe85c11cb07108e630f648fb58b801
Anast2007 <manast5345@gmail.com>
2026-09-21 09:23:38 +0530

    Time: 37 ms (95.07%) | Memory: 71.4 MB (82.96%) - LeetSync

---
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reductionOperations(int[] nums) {
+        Arrays.sort(nums);
+        int ans = 0, count = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i - 1]) count++;
+            ans += count;
+        }
+        return ans;
+    }
+}
