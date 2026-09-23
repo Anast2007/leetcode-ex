7b0d0efb61c7ec9bfc942a397427000578fccabb
Anast2007 <manast5345@gmail.com>
2026-09-23 18:49:19 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (24.35%) - LeetSync

---
+++ 413-arithmetic-slices/arithmetic-slices.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0, count = 0;
+        for(int i = 2; i < nums.length; i++) {
+            if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) count++;
+            else count = 0;
+            ans += count;
+        }
+        return ans;
+    }
+}
